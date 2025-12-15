f872e02eff300c14261f8524696a93ca8c66b694
Piers Karsenbarg <piers@pulumi.com>
2025-12-15 14:35:11 +0000

    checking that the clusterIdentifier is not an empty string

--- __main__.py
+++ __main__.py
@@ -8,11 +8,11 @@ from pulumi_policy import (
 
 def k8s_provider_cluster_identifier_validator(args: ResourceValidationArgs, report_violation: ReportViolation):
     if args.resource_type == "pulumi:providers:kubernetes":
-        if "clusterIdentifier" not in args.props:
+        if "clusterIdentifier" not in args.props or args.props["clusterIdentifier"] == "":
             report_violation("You should set the clusterIdentifier property so that the Kubernetes provider resources will not be replaced until this value is changed.", args.urn)
 
 k8s_provider_cluster_identifier = ResourceValidationPolicy(
-    name="s3-no-public-read",
+    name="k8s-provider-cluster-identifier",
     description="Ensures that the cllusterIdentifier property is set on the Kubernetes provider resource.",
     enforcement_level=EnforcementLevel.ADVISORY,
     validate=k8s_provider_cluster_identifier_validator,
